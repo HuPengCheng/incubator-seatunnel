8a1ea4fb5728cb688fb196dc207388c10a46aab1
pengcheng.hu <pengcheng.hu@yeepay.com>
2025-06-10 09:42:04 +0800

    feat: JdbcSink优化，支持按照批量删除数据而不是全表

--- seatunnel-connectors/seatunnel-connectors-spark/seatunnel-connector-spark-jdbc/src/main/scala/org/apache/seatunnel/spark/jdbc/sink/Jdbc.scala
+++ seatunnel-connectors/seatunnel-connectors-spark/seatunnel-connector-spark-jdbc/src/main/scala/org/apache/seatunnel/spark/jdbc/sink/Jdbc.scala
@@ -18,7 +18,7 @@ package org.apache.seatunnel.spark.jdbc.sink
 
 import scala.collection.JavaConversions._
 import org.apache.seatunnel.common.config.CheckConfigUtil.checkAllExists
-import org.apache.seatunnel.common.config.CheckResult
+import org.apache.seatunnel.common.config.{CheckResult, TypesafeConfigUtils}
 import org.apache.seatunnel.shade.com.typesafe.config.ConfigFactory
 import org.apache.seatunnel.spark.SparkEnvironment
 import org.apache.seatunnel.spark.batch.SparkBatchSink
@@ -26,8 +26,11 @@ import org.apache.spark.sql.{Dataset, Row}
 import org.apache.spark.sql.execution.datasources.jdbc2.{JDBCSaveMode, JdbcOptionsInWrite, JdbcUtils}
 
 import java.sql.SQLException
+import scala.util.{Failure, Success, Try}
 
 class Jdbc extends SparkBatchSink {
+
+  val DELETE_BATCH_SIZE = 10000
 
   override def output(data: Dataset[Row], env: SparkEnvironment): Unit = {
     val saveMode = config.getString("saveMode")
@@ -52,7 +55,20 @@ class Jdbc extends SparkBatchSink {
       prop.setProperty("driver", config.getString("driver"))
       prop.setProperty("user", config.getString("user"))
       prop.setProperty("password", config.getString("password"))
-      data.write.mode(saveMode).jdbc(config.getString("url"), config.getString("dbTable"), prop)
+      val writer = data.write.mode(saveMode)
+      Try(TypesafeConfigUtils.extractSubConfigThrowable(config, "jdbc.", false)) match {
+
+        case Success(options) =>
+          val optionMap = options
+            .entrySet()
+            .foldRight(Map[String, String]())((entry, m) => {
+              m + (entry.getKey -> entry.getValue.unwrapped().toString)
+            })
+
+          writer.options(optionMap)
+        case Failure(_) => // do nothing
+      }
+      writer.jdbc(config.getString("url"), config.getString("dbTable"), prop)
     }
 
   }
@@ -79,8 +95,25 @@ class Jdbc extends SparkBatchSink {
   }
 
   override def cleanDataByEtlIdInSink(env: SparkEnvironment): Unit = {
-    val sql = s"delete from ${config.getString("dbTable")} where ETLTASKID = '${config.getString("task.id")}'"
-    executeSql(sql)
+    val conn = JdbcUtils.createConnectionFactory(config)()
+    val stmt = conn.createStatement()
+    try {
+      val deleteSql = s"delete from ${config.getString("dbTable")} where ETLTASKID = '${config.getString("task.id")}' limit $DELETE_BATCH_SIZE"
+      // 执行删除语句，直到没有数据为止
+      var count = 1
+      while (count > 0) {
+        count = stmt.executeUpdate(deleteSql)
+        if (count > 0) {
+          println(s"${java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))} - Deleted $count rows from ${config.getString("dbTable")} where ETLTASKID = '${config.getString("task.id")}'")
+        }
+      }
+      println(s"${java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))} - All data with ETLTASKID = '${config.getString("task.id")}' has been deleted from ${config.getString("dbTable")}")
+    } catch {
+      case e: SQLException => e.printStackTrace()
+    } finally {
+      stmt.close()
+      conn.close()
+    }
   }
 
   private def executeSql(sql: String) = {
